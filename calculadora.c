bff8263972ffc0b231ea0bb0850d800549ac4bb4
Nicolas Arias <nariasm4uqvirtual.edu.co>
2015-08-13 12:21:48 -0500

    agregar las funciones en calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,19 @@
+int suma(int a, int b)
+{
+return a+b;
+}
+
+int resta(int a, int b)
+{
+return a-b;
+}
+
+int multiplicar(int a, int b)
+{
+return a*b;
+}
+
+int dividir(int a, int b)
+{
+return a/b;
+}
