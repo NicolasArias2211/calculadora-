8bfcfb2c7db49132ac215539ed42aaabba1eb5d3
Nicolas Arias <nariasm4uqvirtual.edu.co>
2015-08-14 17:47:54 -0500

    Se agregó documentación

--- calculadora.h
+++ calculadora.h
@@ -1,5 +1,29 @@
-int sumar(int a,int b);
-int resta(int a,int b);
-int multiplicar(int a,int b);
-int dividir (int a,int b);
+/**
+*\brief Funcion que suma
+*\param Valor1 Operador 1
+*\param Valor2 Operador 2
+*\return Retorna un entero como resultado
+*/
+int sumar(int Valor1,int Valor2);
+/**
+*\brief Funcion que resta
+*\param Valor1 Operador 1
+*\param Valor2 Operador 2
+*\return Retorna un entero como resultado
+*/
+int resta(int Valor1,int Valor2);
+/**
+*\brief Funcion que multiplica
+*\param Valor1 Operador 1
+*\param Valor2 Operador 2
+*\return Retorna un entero como resultado
+*/
+int multiplicar(int Valor1,int Valor2);
+/**
+*\brief Funcion que divide
+*\param Valor1 Operador 1
+*\param Valor2 Operador 2
+*\return Retorna un entero como resultado
+*/
+int dividir (int Valor1,int Valor2);
 
--- main.c
+++ main.c
@@ -1,30 +1,38 @@
 #include<stdio.h>
 #include"calculadora.h"
+
 int main(void)
 {
-int a,b,c,d,op;
-    printf("Que operacion descea realizar:\n 1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n");
-	scanf("%d",& a);
+	int opcion,Valor1,Valor2,Resultado;
+	
+	printf("Que operacion descea realizar:\n 1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n");
+	scanf("%d",&opcion);
 	printf("Ingrese el primer numero a operar");
-	scanf("%d",& b);
+	scanf("%d",& Valor1);
 	printf("Ingrese el segundo numero a operar");
-	scanf(%d",& c);
-switch(a)
-{
-case 1:
-	d=suman(b,c);
-	break;
-case 2:
-	d=resta(b,c);
-	break;
-case 3:
-	d=multiplicar(b,c);
-	break;
-case 4:
-	d=dividir(b,c);
-	break;
-default: 
-	d=0;
-}
-printf("el resultado es %d","d");
+	scanf(%d",& Valor2);
+	
+	switch(opcion)
+	{
+		case 1:
+			Resultado=suman(Valor1,Valor2);
+			break;
+		
+		case 2:
+			Resultado=resta(Valor1,Valor2);
+			break;
+		
+		case 3:
+			Resultado=multiplicar(Valor1,Valor2);
+			break;
+		
+		case 4:
+			Resultado=dividir(Valor1,Valor2);
+			break;
+		
+		default: 
+				Resultado=0;
+	}
+	
+	printf("el resultado es %d","d");
 }
